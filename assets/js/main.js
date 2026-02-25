a55b844bb593bdf7263bbd7662dfec31a7c679f9
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-25 12:14:13 +0100

    fix: incorporate the api logic in a function and prevent duplicate cards on reload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,38 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
 
 const rowEl = document.getElementById('js_row')
-console.log(endpoint, rowEl);
 
-
-
-fetch(endpoint)
-.then(res=>res.json())
-.then(data=> {
-
-    data.forEach(element=>{
-        const {url} = element
-
-        const markup = `
-                <div class="col position-relative">
-            <img class="pin" src="./assets/img/pin.svg"  alt="" />
-            <div class="card rounded-0">
-              <img
-                src="${url}"
-                class="card-img-top pt-3 px-3"
-                alt="..."
-              />
-              <div class="card-body">
-                <p class="card-text pb-3">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+const getApiImages = () =>{
+    rowEl.innerHTML = ''
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(data=> {
+    
+        data.forEach(element=>{
+            const {url} = element
+            const markup = `
+                    <div class="col position-relative">
+                <img class="pin" src="./assets/img/pin.svg"  alt="" />
+                <div class="card rounded-0">
+                  <img
+                    src="${url}"
+                    class="card-img-top pt-3 px-3"
+                    alt="..."
+                  />
+                  <div class="card-body">
+                    <p class="card-text pb-3">
+                      Some quick example text to build on the card title and make up
+                      the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          `
-          console.log(markup);
-          
-          rowEl.insertAdjacentHTML("beforeend", markup)
+              `
+              console.log(markup);
+              
+              rowEl.innerHTML += markup
+        })
     })
-})
+}
 
-
+getApiImages()
