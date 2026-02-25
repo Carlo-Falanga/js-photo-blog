de9969789754511f685c9bf208cda867e647b31d
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-25 11:41:28 +0100

    complete milestone 3 by making the api images appear in the document

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,23 @@
-
-
-/*
-        <div class="col position-relative">
-            <img class="pin" src="/assets/img/pin.svg" alt="" />
-            <div class="card rounded-0">
-              <img
-                src="./assets/img/random-image.jpg"
-                class="card-img-top pt-3 px-3"
-                alt="..."
-              />
-              <div class="card-body">
-                <p class="card-text pb-3">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-
-*/
-
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
 
 const rowEl = document.getElementById('js_row')
 console.log(endpoint, rowEl);
 
+
+
 fetch(endpoint)
 .then(res=>res.json())
 .then(data=> {
-    console.log(data);
 
     data.forEach(element=>{
-        console.log(element);
         const {url} = element
 
         const markup = `
                 <div class="col position-relative">
-            <img class="pin" src= ${url} alt="" />
+            <img class="pin" src="./assets/img/pin.svg"  alt="" />
             <div class="card rounded-0">
               <img
-                src="./assets/img/random-image.jpg"
+                src="${url}"
                 class="card-img-top pt-3 px-3"
                 alt="..."
               />
@@ -54,5 +32,8 @@ fetch(endpoint)
           `
           console.log(markup);
           
+          rowEl.insertAdjacentHTML("beforeend", markup)
     })
 })
+
+
