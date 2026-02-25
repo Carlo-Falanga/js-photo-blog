3efb940b8aa8c1f76c945b0b6f9a1eea98ea0327
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-25 11:11:32 +0100

    remove cards structure in html and save it by commenting it in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+/*
+        <div class="col position-relative">
+            <img class="pin" src="/assets/img/pin.svg" alt="" />
+            <div class="card rounded-0">
+              <img
+                src="./assets/img/random-image.jpg"
+                class="card-img-top pt-3 px-3"
+                alt="..."
+              />
+              <div class="card-body">
+                <p class="card-text pb-3">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+
+*/
