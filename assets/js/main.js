d84097ddc1129892db172cf7513962ba38e09d97
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-25 13:06:06 +0100

    refactor: improve cards layout and use API data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,30 @@
-const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
-const rowEl = document.getElementById('js_row')
+const rowEl = document.getElementById("js_row");
 
-const getApiImages = () =>{
-    rowEl.innerHTML = ''
-    fetch(endpoint)
-    .then(res=>res.json())
-    .then(data=> {
-    
-        data.forEach(element=>{
-            const {url} = element
-            const markup = `
-                    <div class="col position-relative">
-                <img class="pin" src="./assets/img/pin.svg"  alt="" />
-                <div class="card rounded-0">
-                  <img
-                    src="${url}"
-                    class="card-img-top pt-3 px-3"
-                    alt="..."
-                  />
-                  <div class="card-body">
-                    <p class="card-text pb-3">
-                      Some quick example text to build on the card title and make up
-                      the bulk of the card's content.
-                    </p>
-                  </div>
-                </div>
+const getApiImages = () => {
+  rowEl.innerHTML = "";
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        const { url, title, date } = element;
+        const markup = `
+         <div class="col position-relative">
+            <img class="pin" src="./assets/img/pin.svg" alt="" />
+            <div class="card rounded-0 shadow">
+              <img src="${url}" class="card-img-top pt-3 px-3" alt="..." />
+              <div class="card-body">
+                <p class="cards_text card-text mb-0 text-secondary fw-medium">${date}</p>
+                <h5 class="cards_title card-title text-uppercase fw-bolder mb-0">${title}</h5>
               </div>
-              `
-              console.log(markup);
-              
-              rowEl.innerHTML += markup
-        })
-    })
-}
+            </div>
+        </div>           
+            
+        `;
+        rowEl.innerHTML += markup;
+      });
+    });
+};
 
-getApiImages()
+getApiImages();
