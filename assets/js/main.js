2d4ee0db24fdf82d5d1bcb05ce4f608ca6af63a3
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-26 10:28:06 +0100

    get dom elements in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 const rowEl = document.getElementById("js_row");
+const overlayBtnEl = document.getElementById('js_overlay_btn');
+const overlayImgEl = document.getElementById('js_overlay_img')
+const overlayEl = document.querySelector('overlay')
+const cardImgEl = document.getElementById('js_card_img')
 
 const initGallery = () => {
   rowEl.innerHTML = "";
@@ -9,22 +13,23 @@ const initGallery = () => {
     .then((data) => {
       data.forEach((element) => {
         const { url, title, date } = element;
-        const markup = `
+        const markup = 
+        `
          <div class="col position-relative">
             <img class="pin" src="./assets/img/pin.svg" alt="" />
             <div class="card rounded-0 shadow">
-              <img src="${url}" class="card-img-top pt-3 px-3" alt="..." />
+              <img src="${url}" id="js_card_img" class="card-img-top pt-3 px-3" alt="..." />
               <div class="card-body">
                 <p class="cards_text card-text mb-0 text-secondary fw-medium">${date}</p>
                 <h5 class="cards_title card-title text-uppercase fw-bolder mb-0">${title}</h5>
               </div>
             </div>
-        </div>           
-            
-        `;
+        </div>  
+        ` ;
         rowEl.innerHTML += markup;
       });
     });
 };
 
 initGallery();
+
